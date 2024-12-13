4a473abf79b0b79ca624f621052d5b8642f6a33b
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2024-12-13 08:44:33 -0500

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Ahmet Yusuf Yuksek on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
